91c97cad4df6c6899ac1e03670366b5d81518b75
3rumalarao <3rumalarao@gmail.com>
2025-04-06 18:57:45 -0500

    Create main.tf

---
+++ ec2/main.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "this" {
+  for_each = var.instances
+
+  ami                         = each.value.ami
+  instance_type               = each.value.instance_type
+  key_name                    = each.value.key_name
+  subnet_id                   = var.is_public ? var.public_subnets[each.value.subnet_index] : var.private_subnets[each.value.subnet_index]
+  associate_public_ip_address = var.is_public ? false : null
+
+  tags = {
+    Name = "${var.env}-${var.orgname}-${each.key}"
+  }
+}
+
+# Allocate an EIP for public instances
+resource "aws_eip" "this" {
+  for_each = var.is_public ? var.instances : {}
+
+  instance = aws_instance.this[each.key].id
+  vpc      = true
+}
