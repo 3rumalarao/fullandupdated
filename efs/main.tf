4bc0d0af8fe47d947a8f6b05001c06c85ed05c19
3rumalarao <3rumalarao@gmail.com>
2025-04-06 21:59:26 -0500

    Update main.tf

--- efs/main.tf
+++ efs/main.tf
@@ -1,7 +1,7 @@
 resource "aws_efs_file_system" "this" {
   creation_token = var.name
   tags = merge(var.common_tags, {
-    Name = var.name,
+    Name        = var.name,
     Environment = var.environment
   })
 }
@@ -11,5 +11,5 @@ resource "aws_efs_mount_target" "this" {
 
   file_system_id  = aws_efs_file_system.this.id
   subnet_id       = var.private_subnets[each.value.subnet_index]
-  security_groups = []  # You can pass security group IDs as needed
+  security_groups = []  # Adjust if you want to attach a security group
 }
