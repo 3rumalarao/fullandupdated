676b1684029a6d108f5e3698b86ba5346ab56c77
3rumalarao <3rumalarao@gmail.com>
2025-04-06 21:25:23 -0500

    Update variables.tf

--- rds/variables.tf
+++ rds/variables.tf
@@ -1,20 +1,34 @@
-variable "rds" {
+variable "rds_config" {
+  description = "RDS configuration details"
   type = object({
     name           = string
     instance_class = string
     engine         = string
-    username       = string
-    password       = string
     storage        = number
-    subnets        = list(string)
   })
 }
 
-variable "vpc_id" {
-  type = string
+variable "privates_subnets" {
+  description = "List of private subnet IDs for the RDS subnet group"
+  type        = list(string)
 }
 
-variable "sg_id" {
-  description = "Security group ID for the RDS instance"
-  type = string
+variable "environment" {
+  description = "Environment tag"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "Common tags for RDS resources"
+  type        = map(string)
 }
