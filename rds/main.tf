c64b7c6019e469679717f9447ac52b79bd68c6e7
3rumalarao <3rumalarao@gmail.com>
2025-04-09 10:40:07 -0500

    Update main.tf

--- rds/main.tf
+++ rds/main.tf
@@ -1,26 +1,26 @@
 resource "aws_db_subnet_group" "this" {
-  name       = "${var.rds_config.name}-subnet-group"
+  name       = lower("${var.rds_config.name}-subnet-group")
   subnet_ids = var.privates_subnets
 
   tags = merge(var.common_tags, {
-    Name        = "${var.rds_config.name}-subnet-group",
-    Environment = var.environment
+    name        = lower("${var.rds_config.name}-subnet-group"),
+    environment = var.environment
   })
 }
 
 resource "aws_db_instance" "this" {
-  identifier             = var.rds_config.name
+  identifier             = lower("db-${var.rds_config.name}")
   engine                 = var.rds_config.engine
   instance_class         = var.rds_config.instance_class
   username               = var.db_username
   password               = var.db_password
   allocated_storage      = var.rds_config.storage
   db_subnet_group_name   = aws_db_subnet_group.this.name
-  vpc_security_group_ids = []  # Optionally attach security groups
+  vpc_security_group_ids = [var.rds_sg]
   skip_final_snapshot    = true
 
   tags = merge(var.common_tags, {
-    Name        = var.rds_config.name,
-    Environment = var.environment
+    name        = lower("db-${var.rds_config.name}"),
+    environment = var.environment
   })
 }
