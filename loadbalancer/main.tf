bfb7aea26055121fef58aedfaaea6c53b7797098
3rumalarao <3rumalarao@gmail.com>
2025-04-09 10:39:01 -0500

    Update main.tf

--- loadbalancer/main.tf
+++ loadbalancer/main.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "this" {
   name               = var.lb.name
   internal           = var.lb.scheme == "internal"
   load_balancer_type = var.lb.type
-  security_groups    = [var.sg_id]
+  security_groups    = var.lb.security_groups
   subnets            = var.subnet_ids
   tags = {
     Name = var.lb.name
@@ -10,7 +10,7 @@ resource "aws_lb" "this" {
 }
 
 resource "aws_lb_target_group" "this" {
-  name     = "${var.lb.name}-TG"
+  name     = lower("${var.lb.name}-tg")
   port     = var.lb.listener_port
   protocol = var.lb.type == "application" ? "HTTP" : "TCP"
   vpc_id   = var.vpc_id
